23a2d389b802257074cfbd84c94a6c36c4423159
Bakuo <mk.bko56@gmail.com>
2017-03-03 21:57:33 +0900

    Fixed a bug

--- p35_lakeCounting.cpp
+++ p35_lakeCounting.cpp
@@ -22,8 +22,7 @@ void dfs(int y, int x){
 	for(int dy=-1; dy<=1; dy++){
 		for(int dx=-1; dx<=1; dx++){
 			int ny=y+dy, nx=x+dx;
-			if(0<=ny && 0<=nx && ny<n && nx<m && field[ny][nx]=='w'){
-				printf("wow\n");
+			if(0<=ny && 0<=nx && ny<n && nx<m && field[ny][nx]=='W'){
 				dfs(ny, nx);
 			}
 		}
@@ -35,23 +34,19 @@ void dfs(int y, int x){
 
 int main(){
 
-	// I defined n,m here again although they are global var, 
-	// so it coused a problem...
 	std::cin >> n;
 	std::cin >> m;
 
 	for(int y=0; y<n; y++){
 		for(int x=0; x<m; x++){
 			std::cin >> field[y][x];
-			//scanf("%c",&(field[y][x]));
-			printf("%c",field[y][x]);
 		}
 	}
 
 	int ans = 0;
 	for(int y=0; y<n; y++){
 		for(int x=0; x<m; x++){
-			if(field[y][x] == 'w'){
+			if(field[y][x] == 'W'){
 				ans += 1;
 				dfs(y,x);
 			}			
