a980e96fb311cef9dac6a7b23493328d8567f215
Bakuo <mk.bko56@gmail.com>
2017-03-04 22:46:33 +0900

    p37 maze with breadth-first search

---
+++ p37_maze.py
@@ -0,0 +1,37 @@
+N = int( input() )
+M = int( input() )
+maze = [list( input() ) for i in range(N)]
+INF = 10000000
+pathcost = [[INF for i in range(M)] for j in range(N)]
+
+sx = 0
+sy = 0
+
+for i in range(N):
+    for j in range(M):
+        if maze[i][j] == 'S':
+            sx = j
+            sy = i
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+def bfs():
+    que = []
+    que.append([sy,sx])
+    pathcost[sy][sx] = 0
+    while len(que) > 0:
+        y, x = que.pop(0)
+
+        for i, j in zip(dx, dy):
+            ny = y + j
+            nx = x + i
+            if (ny in range(N)) and (nx in range(M)):
+                if maze[ny][nx] == '.' and pathcost[ny][nx]==INF:
+                    que.append([ny,nx])
+                    pathcost[ny][nx] = pathcost[y][x]+1
+                elif maze[ny][nx] == 'G':
+                    pathcost[ny][nx] = pathcost[y][x]+1
+                    return pathcost[ny][nx]
+
+print(bfs())
