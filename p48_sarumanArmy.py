2cebfcc8b55302fa010eef17869a0fad48ae060a
Bakuo <mk.bko56@gmail.com>
2017-03-11 11:54:58 +0900

    p48 saruman army with greedy algorithm

---
+++ p48_sarumanArmy.py
@@ -0,0 +1,22 @@
+
+N = int(input())
+R = int(input())
+x = list(map(int, input().split()))
+
+
+i = 0
+count = 0
+while i < N:
+
+    s = x[i]
+    i += 1
+    while i < N and x[i] - s <= R:
+        i += 1
+
+    count += 1
+    n = x[i-1]
+    
+    while i < N and x[i] - n <= R:
+        i += 1
+
+print(count)
